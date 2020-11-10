95b0a4d8875443e4ce98fc012c4509bef40d831c
Tej Kahlon (Personal) <tpkahlon@Admins-MBP.mshome.net>
2020-11-10 14:50:58 -0500

    fix: add filters

---
+++ common/index.js
@@ -0,0 +1,161 @@
+const processData = (i) => {
+  if (
+    i.getAttribute("href") &&
+    i.getAttribute("title") &&
+    !i.getAttribute("rel") &&
+    !i.getAttribute("accesskey") &&
+    i.textContent.trim() &&
+    !i.className.toLowerCase().includes("interlanguage-link-target") &&
+    !i.getAttribute("title").toLowerCase().includes("3d") &&
+    !i.getAttribute("title").toLowerCase().includes("at&t yahoo!") &&
+    !i.getAttribute("title").toLowerCase().includes("augmented browsing") &&
+    !i.getAttribute("title").toLowerCase().includes("category") &&
+    !i.getAttribute("title").toLowerCase().includes("comparison") &&
+    !i.getAttribute("title").toLowerCase().includes("communicator") &&
+    !i.getAttribute("title").toLowerCase().includes("edit") &&
+    !i.getAttribute("title").toLowerCase().includes("microsoft") &&
+    !i.getAttribute("title").toLowerCase().includes("miro") &&
+    !i.getAttribute("title").toLowerCase().includes("mobile browser") &&
+    !i.getAttribute("title").toLowerCase().includes("mosaic-ck") &&
+    !i.getAttribute("title").toLowerCase().includes("maemo") &&
+    !i.getAttribute("title").toLowerCase().includes("netcaptor") &&
+    !i.getAttribute("title").toLowerCase().includes("nls") &&
+    !i.getAttribute("title").toLowerCase().includes("nokia") &&
+    !i.getAttribute("title").toLowerCase().includes("omni") &&
+    !i.getAttribute("title").toLowerCase().includes("of web") &&
+    !i.getAttribute("title").toLowerCase().includes("qihoo_360") &&
+    !i.getAttribute("title").toLowerCase().includes("research") &&
+    !i.getAttribute("title").toLowerCase().includes("realplayer") &&
+    !i.getAttribute("title").toLowerCase().includes("rockmelt") &&
+    !i.getAttribute("title").toLowerCase().includes("rss") &&
+    !i.getAttribute("title").toLowerCase().includes("tool") &&
+    !i.getAttribute("title").toLowerCase().includes("wikipedia") &&
+    !i.getAttribute("title").toLowerCase().includes("xpcom") &&
+    !i.getAttribute("title").toLowerCase().includes("xul") &&
+    !i.getAttribute("title").toLowerCase().includes("yahoo!") &&
+    !i
+      .getAttribute("title")
+      .toLowerCase()
+      .includes("Whole Internet User's Guide and Catalog") &&
+    !i.getAttribute("href").toLowerCase().includes("_wars") &&
+    !i.getAttribute("href").toLowerCase().includes("_in_a_") &&
+    !i.getAttribute("href").toLowerCase().includes("#") &&
+    !i.getAttribute("href").toLowerCase().includes("(") &&
+    !i.getAttribute("href").toLowerCase().includes("_pre_") &&
+    !i.getAttribute("href").toLowerCase().includes("amigaos") &&
+    !i.getAttribute("href").toLowerCase().includes("autism") &&
+    !i.getAttribute("href").toLowerCase().includes("amazon_kindle") &&
+    !i.getAttribute("href").toLowerCase().includes("apple_inc") &&
+    !i.getAttribute("href").toLowerCase().includes("arpanet") &&
+    !i.getAttribute("href").toLowerCase().includes("ascii") &&
+    !i.getAttribute("href").toLowerCase().includes("aliweb") &&
+    !i.getAttribute("href").toLowerCase().includes("aolpress") &&
+    !i.getAttribute("href").toLowerCase().includes("avast") &&
+    !i.getAttribute("href").toLowerCase().includes("syndrome") &&
+    !i.getAttribute("href").toLowerCase().includes("api") &&
+    !i.getAttribute("href").toLowerCase().includes("applications") &&
+    !i.getAttribute("href").toLowerCase().includes("baidu") &&
+    !i.getAttribute("href").toLowerCase().includes("beos") &&
+    !i.getAttribute("href").toLowerCase().includes("bitnet") &&
+    !i.getAttribute("href").toLowerCase().includes("blogging") &&
+    !i.getAttribute("href").toLowerCase().includes("bolt_") &&
+    !i.getAttribute("href").toLowerCase().includes("browsers") &&
+    !i.getAttribute("href").toLowerCase().includes("browser_synchronization") &&
+    !i.getAttribute("href").toLowerCase().includes("bt_group") &&
+    !i.getAttribute("href").toLowerCase().includes("classilla") &&
+    !i.getAttribute("href").toLowerCase().includes("compuserve") &&
+    !i.getAttribute("href").toLowerCase().includes("cascading_style_sheets") &&
+    !i.getAttribute("href").toLowerCase().includes("cyberdog") &&
+    !i.getAttribute("href").toLowerCase().includes("document_object_model") &&
+    !i.getAttribute("href").toLowerCase().includes("debian") &&
+    !i.getAttribute("href").toLowerCase().includes("dreamcast") &&
+    !i.getAttribute("href").toLowerCase().includes("events") &&
+    !i.getAttribute("href").toLowerCase().includes("emacs") &&
+    !i.getAttribute("href").toLowerCase().includes("email") &&
+    !i.getAttribute("href").toLowerCase().includes("elm") &&
+    !i.getAttribute("href").toLowerCase().includes("extension") &&
+    !i.getAttribute("href").toLowerCase().includes("embedded") &&
+    !i.getAttribute("href").toLowerCase().includes("engine") &&
+    !i.getAttribute("href").toLowerCase().includes("foundation") &&
+    !i.getAttribute("href").toLowerCase().includes("facebook") &&
+    !i.getAttribute("href").toLowerCase().includes("gnuzilla") &&
+    !i.getAttribute("href").toLowerCase().includes("gnome") &&
+    !i.getAttribute("href").toLowerCase().includes("gnu") &&
+    !i
+      .getAttribute("href")
+      .toLowerCase()
+      .includes("graphical_user_interface") &&
+    !i.getAttribute("href").toLowerCase().includes("http") &&
+    !i.getAttribute("href").toLowerCase().includes("history") &&
+    !i.getAttribute("href").toLowerCase().includes("headless_browser") &&
+    !i.getAttribute("href").toLowerCase().includes("hp_veer") &&
+    !i.getAttribute("href").toLowerCase().includes("http") &&
+    !i.getAttribute("href").toLowerCase().includes("hp_touchpad") &&
+    !i.getAttribute("href").toLowerCase().includes("html") &&
+    !i.getAttribute("href").toLowerCase().includes("hypercard") &&
+    !i.getAttribute("href").toLowerCase().includes("ibm_lotus_symphony") &&
+    !i.getAttribute("href").toLowerCase().includes("iceweasel") &&
+    !i.getAttribute("href").toLowerCase().includes("ibox") &&
+    !i.getAttribute("href").toLowerCase().includes("ie_tab") &&
+    !i.getAttribute("href").toLowerCase().includes("internet_explorer_shell") &&
+    !i.getAttribute("href").toLowerCase().includes("javascript") &&
+    !i.getAttribute("href").toLowerCase().includes("kde") &&
+    !i.getAttribute("href").toLowerCase().includes("libwww") &&
+    !i.getAttribute("href").toLowerCase().includes("linux") &&
+    !i.getAttribute("href").toLowerCase().includes("list") &&
+    !i.getAttribute("href").toLowerCase().includes("layout") &&
+    !i.getAttribute("href").toLowerCase().includes("macos") &&
+    !i.getAttribute("href").toLowerCase().includes("mac_os") &&
+    !i.getAttribute("href").toLowerCase().includes("msn_explorer") &&
+    !i.getAttribute("href").toLowerCase().includes("microsoft_windows") &&
+    !i.getAttribute("href").toLowerCase().includes("operating_system") &&
+    !i.getAttribute("href").toLowerCase().includes("private_browsing") &&
+    !i.getAttribute("href").toLowerCase().includes("portal") &&
+    !i.getAttribute("href").toLowerCase().includes("protocol") &&
+    !i.getAttribute("href").toLowerCase().includes("palm_") &&
+    !i.getAttribute("href").toLowerCase().includes("powerpc") &&
+    !i.getAttribute("href").toLowerCase().includes("protocol") &&
+    !i.getAttribute("href").toLowerCase().includes("planetweb") &&
+    !i.getAttribute("href").toLowerCase().includes("plan_9_from_bell_labs") &&
+    !i.getAttribute("href").toLowerCase().includes("qt_webengine") &&
+    !i.getAttribute("href").toLowerCase().includes("risc_os") &&
+    !i.getAttribute("href").toLowerCase().includes("software") &&
+    !i.getAttribute("href").toLowerCase().includes("specific") &&
+    !i.getAttribute("href").toLowerCase().includes("symbian") &&
+    !i.getAttribute("href").toLowerCase().includes("suite") &&
+    !i.getAttribute("href").toLowerCase().includes("software") &&
+    !i.getAttribute("href").toLowerCase().includes("tcl") &&
+    !i.getAttribute("href").toLowerCase().includes("teletext") &&
+    !i.getAttribute("href").toLowerCase().includes("tv") &&
+    !i.getAttribute("href").toLowerCase().includes("telnet") &&
+    !i.getAttribute("href").toLowerCase().includes("tencent_traveler") &&
+    !i.getAttribute("href").toLowerCase().includes("text-based_web_browser") &&
+    !i.getAttribute("href").toLowerCase().includes("twitter") &&
+    !i.getAttribute("href").toLowerCase().includes("university_of_ulm") &&
+    !i.getAttribute("href").toLowerCase().includes("usenet") &&
+    !i.getAttribute("href").toLowerCase().includes("uucp") &&
+    !i.getAttribute("href").toLowerCase().includes("verizon") &&
+    !i.getAttribute("href").toLowerCase().includes("videotex") &&
+    !i.getAttribute("href").toLowerCase().includes("viewdata") &&
+    !i.getAttribute("href").toLowerCase().includes("vrml") &&
+    !i.getAttribute("href").toLowerCase().includes("winamp") &&
+    !i.getAttribute("href").toLowerCase().includes("wikimedia") &&
+    !i.getAttribute("href").toLowerCase().includes("w3c") &&
+    !i.getAttribute("href").toLowerCase().includes("web_browser") &&
+    !i.getAttribute("href").toLowerCase().includes("web_page") &&
+    !i.getAttribute("href").toLowerCase().includes("web_standards") &&
+    !i.getAttribute("href").toLowerCase().includes("web_storage") &&
+    !i.getAttribute("href").toLowerCase().includes("webassembly") &&
+    !i.getAttribute("href").toLowerCase().includes("webgl") &&
+    !i.getAttribute("href").toLowerCase().includes("webkit") &&
+    !i.getAttribute("href").toLowerCase().includes("webrtc") &&
+    !i.getAttribute("href").toLowerCase().includes("websocket") &&
+    !i.getAttribute("href").toLowerCase().includes("world_wide_web") &&
+    !i.getAttribute("href").toLowerCase().includes("worldwideweb") &&
+    !i.getAttribute("href").toLowerCase().includes("x.25")
+  ) {
+    return i;
+  }
+}
+
+export default processData;
--- pages/index.js
+++ pages/index.js
@@ -1,114 +1,43 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { uniqBy } from "lodash";
+import processData from "../common";
+import styles from "../styles/Home.module.css";
 
 export default function Home({ text }) {
-  let r = [];
+  let browsers = [];
   let list = null;
   if (process.browser) {
-    const p = new DOMParser();
-    const dom = p.parseFromString(text, "text/html");
-    [...dom.querySelectorAll("a")]
-      .filter((i) => {
-        if (
-          i.getAttribute("href") &&
-          i.getAttribute("title") &&
-          !i.getAttribute("rel") &&
-          !i.getAttribute("accesskey") &&
-          i.textContent.trim() &&
-          !i.className.includes("interlanguage-link-target") &&
-          !i.getAttribute("title").includes("Category") &&
-          !i.getAttribute("title").includes("Comparison") &&
-          !i.getAttribute("title").includes("Wikipedia") &&
-          !i.getAttribute("title").includes("Edit") &&
-          !i.getAttribute("title").includes("of web") &&
-          !i.getAttribute("title").includes("3DMLW") &&
-          !i.getAttribute("title").includes("AT&T Yahoo!") &&
-          !i.getAttribute("title").includes("Augmented browsing") &&
-          !i.getAttribute("title").includes("XPCOM") &&
-          !i.getAttribute("title").includes("XUL") &&
-          !i.getAttribute("title").includes("Yahoo!") &&
-          !i
-            .getAttribute("title")
-            .includes("Whole Internet User's Guide and Catalog") &&
-          !i.getAttribute("href").includes("X.25") &&
-          !i.getAttribute("href").includes("Autism") &&
-          !i.getAttribute("href").includes("Amazon_Kindle") &&
-          !i.getAttribute("href").includes("Android_(operating_system)") &&
-          !i.getAttribute("href").includes("Apple_Inc.") &&
-          !i.getAttribute("href").includes("ARPANET") &&
-          !i.getAttribute("href").includes("ASCII") &&
-          !i.getAttribute("href").includes("Asperger_syndrome") &&
-          !i.getAttribute("href").includes("Audio_player_(software)") &&
-          !i.getAttribute("href").includes("Microsoft_Windows") &&
-          !i.getAttribute("href").includes("Tasman_(layout_engine)") &&
-          !i.getAttribute("href").includes("Tcl") &&
-          !i.getAttribute("href").includes("InterCon_Systems_Corporation") &&
-          !i.getAttribute("href").includes("Teletext") &&
-          !i.getAttribute("href").includes("Telnet") &&
-          !i.getAttribute("href").includes("Tencent_Traveler") &&
-          !i.getAttribute("href").includes("Text-based_web_browser") &&
-          !i.getAttribute("href").includes("Tor_(anonymity_network)") &&
-          !i.getAttribute("href").includes("HP_TouchPad") &&
-          !i.getAttribute("href").includes("Trident_(software)") &&
-          !i.getAttribute("href").includes("Trident_(layout_engine)") &&
-          !i.getAttribute("href").includes("Twitter") &&
-          !i.getAttribute("href").includes("University_of_Ulm") &&
-          !i.getAttribute("href").includes("Usenet") &&
-          !i.getAttribute("href").includes("UUCP") &&
-          !i.getAttribute("href").includes("HTTP/3") &&
-          !i.getAttribute("href").includes("HTTP/2") &&
-          !i.getAttribute("href").includes("HTML5") &&
-          !i.getAttribute("href").includes("Verizon") &&
-          !i.getAttribute("href").includes("Videotex") &&
-          !i.getAttribute("href").includes("Viewdata") &&
-          !i.getAttribute("href").includes("VRML") &&
-          !i.getAttribute("href").includes("W3C") &&
-          !i.getAttribute("href").includes("Web_browser") &&
-          !i.getAttribute("href").includes("Web_page") &&
-          !i.getAttribute("href").includes("Web_standards") &&
-          !i.getAttribute("href").includes("Web_storage") &&
-          !i.getAttribute("href").includes("WebAssembly") &&
-          !i.getAttribute("href").includes("WebGL") &&
-          !i.getAttribute("href").includes("WebKit") &&
-          !i.getAttribute("href").includes("Palm_webOS") &&
-          !i.getAttribute("href").includes("WebRTC") &&
-          !i.getAttribute("href").includes("WebSocket") &&
-          !i.getAttribute("href").includes("World_Wide_Web") &&
-          !i.getAttribute("href").includes("WorldWideWeb") &&
-          !i.getAttribute("href").includes("AmigaOS")
-        ) {
-          return i;
-        }
-      })
-      .forEach((i) => {
-        r.push({
-          text: i.textContent,
-          url: i.getAttribute("href"),
-        });
+    const parser = new DOMParser();
+    const dom = parser.parseFromString(text, "text/html");
+    [...dom.querySelectorAll("a")].filter(processData).forEach((item) => {
+      browsers.push({
+        text: item.textContent,
+        url: item.getAttribute("href"),
       });
-    r = r.sort((a, b) =>
+    });
+    browsers = browsers.sort((a, b) =>
       a.text.toLowerCase() > b.text.toLowerCase() ? 1 : -1
     );
-    r = uniqBy(r, "text");
-    r = uniqBy(r, "url");
-    console.log(r.length)
+    browsers = uniqBy(browsers, "text");
+    browsers = uniqBy(browsers, "url");
     list = (
       <div className={styles.child}>
-        {r.map((i, ix) => (
+        {browsers.map(({ url, text }, ix) => (
           <div key={ix}>
             <a
-              href={`https://wikipedia.org${i.url}`}
+              href={`https://wikipedia.org${url}`}
               target="_blank"
               rel="noopener noreferrer"
             >
-              {i.text}
+              {text}
             </a>
           </div>
         ))}
       </div>
     );
   }
+  let totalBrowsers = process.browser ? browsers.length : null;
+  let listing = process.browser ? list : null;
   return (
     <div>
       <Head>
@@ -117,31 +46,32 @@ export default function Home({ text }) {
       </Head>
 
       <main className={styles.parent} suppressHydrationWarning={true}>
-        <h1>
-          Total Browsers reported on Wikipedia:{" "}
-          {process.browser ? r.length : null}
-        </h1>
-        <em>
-          Please check back regularly for updated list as I clean up{" "}
-          <a
-            href="https://en.wikipedia.org/wiki/List_of_web_browsers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Wikipedia
-          </a>{" "}
-          list
-        </em>
-        {process.browser ? list : null}
+        <h1>Total Browsers reported on Wikipedia: {totalBrowsers}</h1>
+        <span>
+          You can find the source code{" "}
+          <em>
+            <a
+              href="https://github.com/tpkahlon/browserslist"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+          </em>
+          .
+        </span>
+        {listing}
       </main>
     </div>
   );
 }
 
 Home.getInitialProps = async () => {
-  const r = await fetch(`https://en.wikipedia.org/wiki/List_of_web_browsers`);
-  const t = await r.text();
+  const request = await fetch(
+    `https://en.wikipedia.org/wiki/List_of_web_browsers`
+  );
+  const text = await request.text();
   return {
-    text: t,
+    text,
   };
 };
